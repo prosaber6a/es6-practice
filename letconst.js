17a4bf960f2983d839c5d7ac7a5c109499c0d3d0
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-12 19:23:40 +0600

    let & const

---
+++ letconst.js
@@ -0,0 +1,29 @@
+const hubby = "Elias Kanson";
+// hubby = "Omar Sunny";
+console.log(hubby);
+
+const number = [12, 45];
+number[1] = 88;
+number.push(20);
+// number = ["Omar", "Elias", "Omit"];
+console.log(number);
+
+const nayak = {
+    name : "Sakib Al Hasan",
+    phone: 4578
+};
+nayak.name = "Saniyad Saif Xisan";
+nayak.phone = [12, 58, 78, 96];
+console.log(nayak);
+
+let patientName = "Abdur Rahim";
+patientName = "Fatema Begum";
+console.log(patientName);
+// var is global variable
+// let is local variable
+let sum = 20;
+for (let i = 0; i < 10; i++) {
+    sum = sum + 1
+    
+}
+console.log(sum);
