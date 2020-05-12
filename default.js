d1b8b7879bc60a6032ee6f9073bdb47eda46089a
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-12 21:28:32 +0600

    parameter default value

---
+++ default.js
@@ -0,0 +1,10 @@
+function addNumber (num1, num2 = 0) {
+    // if (num2 == undefined) {
+    //     num2 = 0;
+    // }
+    // num2 = num2 || 0;
+    return num1 + num2;
+}
+
+const total = addNumber(15);
+console.log(total);
