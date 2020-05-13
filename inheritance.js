8396cce6161eed02683f168ecf12147daa3965a1
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-13 11:39:44 +0600

    Inheritance

---
+++ inheritance.js
@@ -0,0 +1,22 @@
+class Parent {
+    constructor () {
+        this.father = "Schwerznegger";
+    }
+}
+
+class Child extends Parent {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+    getFullName() {
+
+        return this.name + " " + this.father;
+    }
+}
+
+const baby = new Child("Arnold");
+const baby2 = new Child("Tom");
+console.log(baby);
+console.log(baby.getFullName());
+console.log(baby2);
